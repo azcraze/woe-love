560b621546fd51b698fd8f1ddf0457c410644210
azcraze <15200975+azcraze@users.noreply.github.com>
2023-03-27 12:53:27 -0500

    :package: :sparkles: feat(code): Err module written
    created the src and lib directories

---
+++ index.js
@@ -0,0 +1,26 @@
+/* --------------- Pretty Errors -------------- */
+var PrettyError = require('pretty-error');
+var pe = new PrettyError();
+
+require('dotenv').config()
+
+/* ------------- External Packages ------------ */
+
+
+
+
+
+
+/* ------------------- Paths ------------------ */
+const PKG_ROOT = './';
+const PKG_ENTRY = './index.js'
+
+
+/* ------------------- Misc ------------------- */
+
+
+module.exports = {
+    duck,
+    PKG_ROOT,
+    PKG_ENTRY
+}
