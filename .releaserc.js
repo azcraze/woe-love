5da5bf5f180cd9dd41df4b0d7da085d8fb64b98a
azcraze <15200975+azcraze@users.noreply.github.com>
2023-03-27 03:49:06 -0500

    :green_heart: :recycle: refactor: Semantic release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,23 +1,8 @@
-module.exports = {
-    "plugins": [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/git",
-      [
-        "@semantic-release/release-notes-generator",
-        {
-          "preset": "angular",
-          "writerOpts": {
-            "commitsSort": ["subject", "scope"]
-          },
-          "template": "./path/to/release-notes-template.hbs"
-        }
-      ],
-      "@semantic-release/github"
-    ],
-    "git": {
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${body}\n\n${footer}\n",
-      "assets": ["CHANGELOG.md"],
-      "commitHooks": ["npm run lint"]
-    }
-  }
-  
+{
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git"
+  ]
+}
