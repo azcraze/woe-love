e6b4773a0970d1a00422918a949c95dce82c383d
azcraze <15200975+azcraze@users.noreply.github.com>
2023-03-27 22:34:18 -0500

    :sparkles: :sparkles: feat(code): Added core/input and core/scoresHandler scripts

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 /* --------------- Pretty Errors -------------- */
 var PrettyError = require('pretty-error');
 var pe = new PrettyError();
-
+require('requireindex')
 require('dotenv').config()
 
-/* ------------- External Packages ------------ */
+/* ------------- Packages ------------ */
 
-
+const { core, err, filesystem, misc, store, utils } = require('./lib')
 
 
 
 
 /* ------------------- Paths ------------------ */
 const PKG_ROOT = './';
-const PKG_ENTRY = './index.js'
+const PKG_ENTRY = './index'
 
 
 /* ------------------- Misc ------------------- */
+
+const duck = `  __
+<(o )___
+ ( ._> /
+  \`---'
+`;
 
 
 module.exports = {
     duck,
     PKG_ROOT,
-    PKG_ENTRY
+    PKG_ENTRY,
+    log: err.log,
+    logger: err.logger,
+    errors: err.errors,
+    is: err.is,
+    check: core.check,    
+    addScores: core.addScores,
+    removeScores: core.removeScores,
+    argCount: core.argCount,
+    sanitize: core.sanitize,
 }
